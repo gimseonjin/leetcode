926c13c6f9df3e2283f8abf1d4ce14be04258b7e
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-10-26 23:10:06 +0900

    Time: 288 ms (5.25%), Space: 15.4 MB (92.90%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -10,7 +10,6 @@ first make binary search
 
 and find the target's index!
 
-"""
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
@@ -24,7 +23,6 @@ class Solution:
             else:
                 return [-1,-1]
     
-        
         result = []
         
         left, right = 0, len(nums)-1
@@ -51,6 +49,44 @@ class Solution:
                 break
         
         return result if result else [-1, -1]
+
+"""
+
+class Solution:
+    
+    def __init__(self):
+        self.start = 0
+        self.end = 0
+        self.is_find = False
+    
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
         
+        if len(nums) == 0:
+            return [-1,-1]
+        
+        if len(nums) == 1: 
+            if nums[0] == target:
+                return [0,0]
+            else:
+                return [-1,-1]
+        
+        self.start, self.end = len(nums), -1
+        
+        def trees (n: list[int], target: int, mid: int):
+            if len(n) == 1:
+                if n[0] == target:
+                    self.start = min(self.start, mid)
+                    self.end = max(self.end, mid)
+                    self.is_find = True
+                return
+            
+            m = int(len(n) / 2 + 0.5)
+            
+            trees(n[:m], target, mid)
+            trees(n[m:], target, mid+m)
+        
+        trees(nums, target, 0)
+        
+        return [self.start, self.end] if self.is_find else [-1,-1]
                 
         
