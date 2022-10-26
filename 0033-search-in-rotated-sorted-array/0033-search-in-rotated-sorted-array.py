925b4ec64a9cc03ce28f97844868f81dafce4c9f
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-10-27 06:15:29 +0900

    Time: 60 ms (75.90%), Space: 14.4 MB (18.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,20 +1,42 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        
+        def find_k(nums):
+            k = 0
+            
+            if nums[0] < nums[len(nums)-1]:
+                return k
+            
+            while k+1 < len(nums) and nums[k] < nums[k+1]:
+                k += 1
+                
+            return k+1
+        
+        def rotation(nums, k):
+            return nums[k:] + nums[:k]
+        
+        if len(nums) == 0:
+            return -1
+        
+        if len(nums) == 1:
+            if nums[0] == target:
+                return 0
+            else:
+                return -1
+        
+        k = find_k(nums)
+        result = rotation(nums, k)
+        
+        left, right = 0, len(nums)-1
+        
         while left <= right:
+            mid = int((left+right)/ 2 + 0.5)
             
-            mid = left + (right - left)//2
-            if nums[mid] == target:
-                return mid
-            
-            elif nums[mid] >= nums[left]: # left sub-array is non-rotated
-                if target >= nums[left] and target < nums[mid]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            else: # right sub-array is non-rotated
-                if target <= nums[right] and target > nums[mid]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+            if result[mid] > target:
+                right = mid-1
+            elif result[mid] < target:
+                left = mid+1
+            else:
+                return (mid + k) % len(nums)
+        
         return -1
